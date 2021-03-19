2ca84d8287291971e02e1b34c0224f3ae896cdf7
zhengwujia <418894252@qq.com>
2021-03-19 10:17:33 +0800

    feat(前端用户页面): 加入layuimini，并成功调试前后端登录接口

---
+++ src/main/java/com/imooc/activitiweb/controller/ActivitSecurityController.java
@@ -0,0 +1,20 @@
+package com.imooc.activitiweb.controller;
+
+
+import com.imooc.activitiweb.util.AjaxResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author jzwu
+ * @since 2021/3/18 0018
+ */
+public class ActivitSecurityController {
+
+   /* @RequestMapping("/login")
+    public String requireAuthenication(HttpServletRequest request, HttpServletResponse response) {
+        return new String("需要登陆,请使用login.html或发起post请求");
+    }*/
+}
